67b4c2aa63538659183a964270f7d9ea57568c89
KKLTU <kkhalaf@ltu.edu>
2016-03-30 21:13:09 -0400

    On Button Click, V runs forever while(true)

--- MPConBot-PC/UdpServer.cs
+++ MPConBot-PC/UdpServer.cs
@@ -1,56 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MPConBot
 {
-// This class is responsible of running the server side on the PC. 
     class UdpServer
     {
+        bool VideoStreamStatus = false;
+
         static void Main(string[] args)
         {
-            byte[] data = new byte[1024];
+            Capture capture = new Capture();
+            Image<Bgr, Byte> frame;
+            byte[] dataToSend;
+            byte[] dataReceived = new byte[1024];
+
+            var VideoStreamToken = new CancellationTokenSource(); //create token for the cancel
+
             UdpClient serverSocket = new UdpClient(15000);
+
+            //string LocalImagePath = "C:\\Users\\kkhalaf\\Desktop\\Phone.PNG";                                 // image path
+            //FileStream fs = new FileStream(LocalImagePath, FileMode.Open, FileAccess.Read);                   // prepare
+            //BinaryReader br = new BinaryReader(fs);                                                           // prepare
+            //byte[] dataToSend = br.ReadBytes(Convert.ToInt16(fs.Length));                                     //Convert image to byte[]
+
             int i = 0;
-
-            while (true)
+            while (true) // this while for keeping the server "listening"
             {
-                Console.WriteLine("Waiting for a UDP client...");
-                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-                data = serverSocket.Receive(ref sender);
+                Console.WriteLine("Waiting for a UDP client...");                                   // display stuff
+                IPEndPoint client = new IPEndPoint(IPAddress.Any, 0);                               // prepare
+                dataReceived = serverSocket.Receive(ref client);                                    // receive packet
+                string stringData = Encoding.ASCII.GetString(dataReceived, 0, dataReceived.Length); // get string from packet
+                Console.WriteLine("Response from " + client.Address);                               // display stuff
+                Console.WriteLine("Message " + i++ + ": " + stringData + "\n");                     // display client's string
 
-                string stringData = Encoding.ASCII.GetString(data, 0, data.Length);
-                Console.WriteLine("Response from " + sender.Address);
-                Console.WriteLine("Message " + i++ + ": " + stringData + "\n");
-
-                // Here I am sending back
-                byte[] data2 = Encoding.ASCII.GetBytes("Response");
-                serverSocket.Send(data2, 8, sender);
-                // try using sockets
+                //Capture capture = new Capture();
+                //Image<Bgr, Byte> frame;
+                //byte[] dataToSend;
+                while (true)
+                {
+                    frame = capture.QueryFrame();
+                    var ms = new MemoryStream();
+                    frame.Bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);                  // save it in the memory stream
+                    dataToSend = ms.ToArray();                                                      // convert to byte 
+                    serverSocket.Send(dataToSend, dataToSend.Length, client);                       // Here I am sending back
+                }
             }
-
         }
+
+//        if(stringData == "StartVideo")
+//                {
+//                    VideoStreamToken = new CancellationTokenSource();
+//        Task.Run(() => StartVideoStream(ref serverSocket, ref client), VideoStreamToken.Token); //start method on another thread
+//                }
+//                if(stringData == "StopVideo")
+//                {
+//                    VideoStreamToken.Cancel();
+//                    serverSocket.Close();
+//                }
+//static public void StartVideoStream(ref UdpClient serverSocket, ref IPEndPoint client)
+//        {
+//            Capture capture = new Capture();
+//            Image<Bgr, Byte> frame;
+//            byte[] dataToSend;
+//            while (true)
+//            {
+//                frame = capture.QueryFrame();
+//                var ms = new MemoryStream();
+//                frame.Bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);                  // save it in the memory stream
+//                dataToSend = ms.ToArray();                                                      // convert to byte 
+//                serverSocket.Send(dataToSend, dataToSend.Length, client);                       // Here I am sending back
+//            }
+//        }
     }
-
-
 }
 
 
-//byte[] UdpPacket = new byte[1024];
-//UdpClient serverSocket = new UdpClient(15000);
-//int id = 0;
-
-//            while (true)
-//            {
-//                Console.WriteLine("Waiting for a UDP client...");
-//                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-//UdpPacket = serverSocket.Receive(ref sender);
-
-//                string stringInPacket = Encoding.ASCII.GetString(UdpPacket, 0, UdpPacket.Length);
-//Console.WriteLine("Response from " + sender.Address);
-//                Console.WriteLine("Message " + id++ + ": " + stringInPacket + "\n");
-//            }
